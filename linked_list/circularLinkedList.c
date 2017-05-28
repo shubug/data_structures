075959c2c203767c05d225428b65e4438fef0c7a
shubug <shubug7320@gmail.com>
2017-05-28 19:55:15 +0530

    finding Leader function using Morpheus Cycle (Delete every Mth node)

--- linked_list/circularLinkedList.c
+++ linked_list/circularLinkedList.c
@@ -34,6 +34,24 @@ void printlist(listNode *head){
   printf("%d ", temp->data);
   temp = temp->next;
  }
+ printf("\n");
+}
+
+listNode *morpheusCycle(listNode **head, int n, int m){
+ int i;
+ listNode *temp = *head;
+ for(i=1;i<n;i++){
+  int M=m-1; 
+  while(M>1){
+   temp=temp->next;
+   M--;
+  }
+  listNode *temp2 = temp->next;
+  printf("Deleted Node: %d\n", temp2->data);
+  temp->next=temp2->next; //This extra temp2 variable is needed b'coz direct temp->next->next is showing some struct typedef incompatibility error
+  temp = temp->next;
+ }
+ return temp;
 }
 
 int main(){
@@ -41,7 +59,12 @@ int main(){
  addNode(&head, 2);
  addNode(&head, 4);
  addNode(&head, 6);
+ addNode(&head, 3);
+ addNode(&head, 11);
+ addNode(&head, 5);
  printlist(head);
  
+ listNode *res = morpheusCycle(&head, 6, 3);
+ printf("last node: %d\n", res->data); //here N=6 is the length of the list and M=3
  return 0;
 }
