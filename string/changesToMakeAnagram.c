aeb7eba4e923be2602e45d7868c7bbb2597b04d2
shubug <shubug7320@gmail.com>
2017-06-04 23:58:33 +0530

    Changes in template function

--- string/changesToMakeAnagram.c
+++ string/changesToMakeAnagram.c
@@ -4,6 +4,23 @@
 #include <stdlib.h>
 #include <math.h>
 
+int arr[26] = {0};
+
+int numOfChanges(char str[], int len){
+ int i;
+ char *c;
+ c = str
+ 
+ for(i=0; i<len/2; i++, c++){
+  arr[(*c)-'a']++;
+  
+ c++;
+ while(*c != '\0'){
+ } 
+
+ return 0;
+}
+
 int main(){
  char str[10001];
  int len, count;
