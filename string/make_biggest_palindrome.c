088125874673dd262fed3b1e8d7aed15316bdf30
shubug <shubug7320@gmail.com>
2017-06-06 23:59:05 +0530

    Adding comments in making bigger palindrome string function

--- string/make_biggest_palindrome.c
+++ string/make_biggest_palindrome.c
@@ -1,3 +1,4 @@
+/*** Make biggest palidrome string from the given string with the fixed number of changes <= k ***/
 #include <stdio.h>
 #include <string.h>
 
@@ -16,7 +17,7 @@ int main(){
   j = i + 2;
 
  if(k > 0){
-  while(j<n && i>=0){
+  while(j<n && i>=0){ //Changes in this while loop are neccessary to make the resultant string a palindrome if it is already not
    if(str[j]-'0' > str[i]-'0'){
     str[i] = str[j];
     if(arr[i] == 0){
@@ -35,7 +36,7 @@ int main(){
    i--;
    j++;
   
-   if(k == 0){
+   if(k == 0){ //To check if after changing some characters we are short of any more changes,  then just check if remaining corresponding characters are matching or not
     while((str[i] == str[j]) && j<n){
      i--;
      j++; 
@@ -51,17 +52,18 @@ int main(){
    }
     
   }
+  //At this point,  string is already palindrome but we are left with more changes (k>0) so we will try to convert remaining characters in '9' as much as we can
   i = 0;
   j = n-1;
   while(k>=1 && i<j){
-   if(arr[i]==1 || arr[j]==1){
+   if(arr[i]==1 || arr[j]==1){//to make both corresponding chars '9',but we have already touched/changed one of them, so no need to decrement 'k' (no need to count it as new change)
     if(str[i] != '9'){ 
      str[i] = str[j] = '9';
      k--;
     }
    }
    else{
-    if(k == 1)
+    if(k == 1) //only one change is left, but to make the string bigger we need atleast 2 changes k>=2
      break;
     if(str[i] != '9'){
      str[i] = '9';
@@ -80,11 +82,11 @@ int main(){
    j--;
   }
   
-  if(k>0 && n%2!=0)
+  if(k>0 && n%2!=0) //if k=1 so and string has odd length , we try to change middle character as '9'
    str[n/2] = '9';
   printf("%s\n", str);
  }
- else{
+ else{ //If number of changes allowed are 0, then just check if the corresponding characters are matching than string is already palindrome ,return it as it is, else return -1 
   while(i>=0 && j<n){
    if(str[i] != str[j]){
     printf("-1\n");
