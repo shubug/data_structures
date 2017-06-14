228170e81b07283098c4ef27519910fd2332c67b
shubug <shubug7320@gmail.com>
2017-06-14 17:04:45 +0530

    Additional function to print all the replace/insert/remove operations needed in edit distance problem

--- dynamic_programming/edit_distance.c
+++ dynamic_programming/edit_distance.c
@@ -26,7 +26,7 @@ int editDistanceUsingDP(char str1[], int m, char str2[], int n){
   return m>0?m:n;
 
  int arr[n+1][m+1];
- int i, j;
+ int i, j, res;
 
  for(i=0; i<=n; i++)
   arr[i][0] = i;
@@ -42,12 +42,56 @@ int editDistanceUsingDP(char str1[], int m, char str2[], int n){
   }
  }
 
- return arr[i-1][j-1];
+ res = arr[i-1][j-1];
+ /** Additional code to print the changes needed to be done to convert one string into other **/
+  int replace, remove, insert;
+  i--;
+  j--;
+  while(i>0 && j>0){
+   if(str1[j-1] == str2[i-1]){
+    //Nothing to print..no operation is performed
+    i=i-1;
+    j=j-1;
+   }
+   else{
+    replace = arr[i-1][j-1];
+    insert = arr[i-1][j];
+    remove = arr[i][j-1];
+
+    if(replace < insert){
+     if(replace < remove){
+      //replace is the minimum cost
+      printf("Replace %c->%c\n", str1[j-1], str2[i-1]);
+      i--;
+      j--;
+     }
+     else{
+      //remove is the minimum cost
+      printf("Remove %c\n", str1[j-1]);
+      j--;
+     }
+    }
+    else{
+     if(insert < remove){
+      //insert is the minimum cost
+      printf("Insert %c\n", str2[i-1]);
+      i--;
+     }
+     else{
+      //remove is the minimum cost
+      printf("Remove %c\n", str1[j-1]);
+      j--;
+     }
+    }
+   }
+  }
+
+ return res;
 }
 
 int main(){
- char str1[] = "sunday";
- char str2[] = "saturday";
+ char str1[] = "saturday";
+ char str2[] = "sunday";
  int m = strlen(str1);
  int n = strlen(str2);
  
